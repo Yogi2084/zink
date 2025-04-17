9c81c28548716a9ab01f5184700406599956c8dd
Yogi2084 <1si22is406@sit.ac.in>
2025-04-17 12:04:25 +0530

    Add Footer component with social media links and update layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-
+// app/layout.tsx
 import './globals.css'
 import { ReactNode } from 'react'
+
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white font-sans">
+      <body className=" text-white font-sans">
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-
+// app/page.tsx
 import NavBar from '@/components/NavBar';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <>
-      <NavBar />
+    <><div><NavBar /></div>
+      
       <main>
       </main>
+      <div className="flex flex-col items-center justify-center min-h-screen text-white"/>
+      <Footer />
     </>
   )
 }
